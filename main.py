399b43870da804bc8480a27a05786800a5cf83ea
energetiq <energetiq@outlook.com>
2023-12-18 21:33:35 +0100

    output fonctionnel

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import scapy.all as sc
 pcap_file = "Ressources SAE101/Wireshark/McDiarmid.pcapng"
-output_file = "Ressources SAE101/Wireshark/output.txt"
+output_file = "Ressources SAE101/Wireshark/output.pdf"
 packets = sc.rdpcap(pcap_file)
 
 ftp_data = b''
 for packet in packets:
-    if 'Raw' in packet and 'FTP-DATA' in packet:
+    if 'Raw' in packet :
         ftp_data += bytes(packet['Raw'].load)
 
 with open(output_file, 'wb') as f:
-    f.write(ftp_data)
+    f.write(ftp_data) 
